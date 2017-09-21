62cf667e8b30e0f0db1ca27dfb6aa2d8ce9624d0
pbrPOPRN <helder.gabriel@pop-rn.rnp.br>
2017-09-21 10:54:45 -0300

    create table for bilhetes

--- pbrVenv/pbr/pbr/settings.py
+++ pbrVenv/pbr/pbr/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
-    'bootstrap3',
+    'bootstrap4',
 
 ]
 
--- pbrVenv/pbr/pbr/urls.py
+++ pbrVenv/pbr/pbr/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import url, include
 from django.contrib import admin
-from core.views import bilhetef
+from core import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'formulario/$', bilhetef, name ='bilhetef'),
+    url(r'formulario/$', views.bilhetef, name ='bilhetef'),
+    url(r'lista/$',views.lista, name='lista'),
+
+
 ]
