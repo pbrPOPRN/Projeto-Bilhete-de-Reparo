b4c2a7de8e985f353ff86233ef585c25ed10e486
pbrPOPRN <30290947+pbrPOPRN@users.noreply.github.com>
2017-10-19 07:37:34 -0300

    arrumando

--- PBR/PBR/PBR/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url, include
-from django.contrib import admin
-from core.views import bilhetef
-
-urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'formulario/$', bilhetef, name ='bilhetef'),
-]
--- PBR/PBR/core/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls import url
-from core.views import home
-
-urlpatterns = [
- url(r'^$', home)
-]
